c5da803513d327a5e77233c754c59e27774e5dda
qctrung <lngquoctrung@gmail.com>
2025-10-04 14:16:43 +0700

    downgrade streamlit from 1.35.0 to 1.32.0

--- app/utils/ui_components.py
+++ app/utils/ui_components.py
@@ -5,7 +5,7 @@ def setup_page_config():
     st.set_page_config(
         page_title="Diabetes Prediction Dashboard",
         layout="wide",
-        initial_sidebar_state="expanded"
+        initial_sidebar_state="collapsed"
     )
 
 def add_custom_css():
