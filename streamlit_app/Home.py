9d6146eab07711e42972f05833a4eb2b7df9d6dd
qctrung <lngquoctrung@gmail.com>
2025-10-11 04:52:31 +0700

    Using subprocess instead of setup.sh

--- streamlit_app/Home.py
+++ streamlit_app/Home.py
@@ -1,32 +1,32 @@
 # Setup before running app
+import os
 import subprocess
-subprocess.run(
-    ["mkdir", "-p", "~/services/public/diabetes-prediction-app"]
-    , check=True)
-subprocess.run(
-    ["curl", 
-     "https://github.com/lngquoctrung/diabetes-prediction/releases/download/v1.0.0/xgboost_model_checkpoint.pkl", 
-     "-o", 
-     "~/services/public/diabetes-prediction-app/xgboost_model_checkpoint.pkl"
-    ], 
-    check=True)
-subprocess.run(
-    ["curl", 
-     "https://github.com/lngquoctrung/diabetes-prediction/releases/download/v1.0.0/xgb_min_max_scaler.pkl", 
-     "-o", 
-     "~/services/public/diabetes-prediction-app/xgb_min_max_scaler.pkl"
-    ], 
-    check=True)
-subprocess.run(
-    ["curl", 
-     "https://github.com/lngquoctrung/diabetes-prediction/releases/download/v1.0.0/brfss_dataset.csv", "-o", 
-     "~/services/public/diabetes-prediction-app/brfss_dataset.csv"
-    ], 
-     check=True)
 
+home_dir = os.path.expanduser("~")
+target_dir = os.path.join(home_dir, "services/public/diabetes-prediction-app")
+
+os.makedirs(target_dir, exist_ok=True)
+
+files_to_download = {
+    "xgboost_model_checkpoint.pkl": "https://github.com/lngquoctrung/diabetes-prediction/releases/download/v1.0.0/xgboost_model_checkpoint.pkl",
+    "xgb_min_max_scaler.pkl": "https://github.com/lngquoctrung/diabetes-prediction/releases/download/v1.0.0/xgb_min_max_scaler.pkl",
+    "brfss_dataset.csv": "https://github.com/lngquoctrung/diabetes-prediction/releases/download/v1.0.0/brfss_dataset.csv"
+}
+
+for filename, url in files_to_download.items():
+    dest_path = os.path.join(target_dir, filename)
+    if not os.path.exists(dest_path):
+        try:
+            subprocess.run(["curl", "-L", url, "-o", dest_path], check=True)
+            print(f"Downloaded {filename}")
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to download {filename}: {e}")
+    else:
+        print(f"{filename} already exists. Skipping download.")
+
+# Home page
 import streamlit as st
 import sys
-import os
 
 from pathlib import Path
 
